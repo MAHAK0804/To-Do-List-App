28da19491f755d71a30cd0ff27ed31f6f51b903a
MAHAK <mahak191713@gmail.com>
2023-08-24 22:55:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'To-DoList';
+  list:any[]=[];
+  addtask(item:string){
+    this.list.push({id:this.list.length,name:item});
+    console.log(this.list);
+  }
+  removetask(id:number){
+    console.log(id);
+    this.list=this.list.filter(item=> item.id!=id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {NgFor} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    FormsModule, MatFormFieldModule, MatInputModule,
+    MatListModule, NgFor
   ],
   providers: [],
   bootstrap: [AppComponent]
